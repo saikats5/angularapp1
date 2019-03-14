6331e388a31952a6a90acc5f6c371ecb828315b9
Saikat <saikats_5@yahoo.co.in>
2019-03-14 08:03:26 +0530

    angular routing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # angularapp1
+
+https://www.truecodex.com/course/angular-project-training/project-overview-and-setup-angular
+ng generate module abc --routing  // only along with module
+ keep approuting module at the end
--- proj/src/app/app.module.ts
+++ proj/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BlogpostModule } from './blogpost/blogpost.module';
+import { CmspageModule } from './cmspage/cmspage.module';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { BlogpostFeaturedComponent } from './blogpost/blogpost-featured/blogpost-featured.component';
+/* import { BlogpostFeaturedComponent } from './blogpost/blogpost-featured/blogpost-featured.component';
 import { BlogpostListComponent } from './blogpost/blogpost-list/blogpost-list.component';
 import { BlogpostDetailComponent } from './blogpost/blogpost-detail/blogpost-detail.component';
 import { BlogpostRecentComponent } from './blogpost/blogpost-recent/blogpost-recent.component';
-import { CategoriesComponent } from './blogpost/categories/categories.component';
+import { CategoriesComponent } from './blogpost/categories/categories.component'; */
 
 @NgModule({
   declarations: [
@@ -19,14 +23,17 @@ import { CategoriesComponent } from './blogpost/categories/categories.component'
     FooterComponent,
     BannerComponent,
     PageNotFoundComponent,
-    BlogpostFeaturedComponent,
+/*     BlogpostFeaturedComponent,
     BlogpostListComponent,
     BlogpostDetailComponent,
     BlogpostRecentComponent,
-    CategoriesComponent
+    CategoriesComponent */
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BlogpostModule,
+    CmspageModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
