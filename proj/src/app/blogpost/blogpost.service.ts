650821020bf86913bfd8f027aacb50c6911947ca
Saikat <saikats_5@yahoo.co.in>
2019-03-17 18:25:50 +0530

    changes

--- proj/src/app/blogpost/blogpost.service.ts
+++ proj/src/app/blogpost/blogpost.service.ts
@@ -9,14 +9,44 @@ import { catchError } from 'rxjs/operators';
 })
 export class BlogpostService {
 
-  ServerUrl = "http://localhost/dev/blogger/";
+  ServerUrl = 'http://localhost/dev/blogger/';
   errorData: {};
 
   constructor(private http: HttpClient) { }
 
-  getBlogs(){
+  getBlogs() {
     return this.http.get<Blogpost>(this.ServerUrl + 'api/blogs').pipe(
-      catchError(this.handleError);
-    )
+      catchError(this.handleError)
+    );
+  }
+
+  getFeaturedBlogs() {
+    return this.http.get<Blogpost>(this.ServerUrl + 'api/featured_blogs').pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+
+      // A client-side or network error occurred. Handle it accordingly.
+
+      console.error('An error occurred:', error.error.message);
+    } else {
+
+      // The backend returned an unsuccessful response code.
+
+      // The response body may contain clues as to what went wrong,
+
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+
+    // return an observable with a user-facing error message
+
+    this.errorData = {
+      errorTitle: 'Oops! Request for document failed',
+      errorDesc: 'Something bad happened. Please try again later.'
+    };
+    return throwError(this.errorData);
   }
 }
